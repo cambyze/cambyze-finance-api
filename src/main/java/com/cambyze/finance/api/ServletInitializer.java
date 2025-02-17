f83c9815f6e9cca98ce7439f232cfbb66ace8596
cambyze <cambyze@gmail.com>
2025-02-17 17:48:29 +0100

    Fix the structure of the file ServletInitializer.java
    Signed-off-by: cambyze <cambyze@gmail.com>

--- src/main/java/com/cambyze/finance/api/ServletInitializer.java
+++ src/main/java/com/cambyze/finance/api/ServletInitializer.java
@@ -1,11 +1,11 @@
 package com.cambyze.finance.api;
 
-/**
- * Generated Spring Servlet Initializer
- */
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Generated Spring Servlet Initializer
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
   @Override
